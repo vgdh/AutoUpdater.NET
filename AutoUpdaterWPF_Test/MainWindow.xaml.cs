8a9b3e18c178a9c5826307a58ba74feef499a8e6
Gus <G@G.r>
2016-11-21 11:57:29 +0500

    Good

--- AutoUpdaterWPF_Test/MainWindow.xaml.cs
+++ AutoUpdaterWPF_Test/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace AutoUpdaterWPF_Test
         public MainWindow()
         {
             InitializeComponent();
-            AutoUpdater.Initialize("https://dl.dropboxusercontent.com/u/23825089/Update.xml", 60, UpdateType.CheckOnly);
+            AutoUpdater.Initialize("https://dl.dropboxusercontent.com/u/23825089/Update.xml", 60 * 10, UpdateType.UpdateWindow);
             AutoUpdater.UpdateCheckEvent += AutoUpdaterOnUpdateAvailableNotify;
         }
 
